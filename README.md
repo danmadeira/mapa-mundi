afdb4e9782b400b8b8e428aae03f21a2f59853f7
Daniel Madeira <dmadeira@gmail.com>
2021-01-13 00:59:23 -0300

    projeções e grade

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Um mapa da superfície total da Terra, projetado a partir dos dados fornecidos p
 
 O Natural Earth fornece arquivos, no formato GeoJSON, com as coordenadas geográficas de todo o perímetro de cada país. Este script em PHP, converte estas coordenadas geográficas em pontos para a construção dos polígonos em SVG. A base do SimpleMaps foi importada para o banco de dados MySQL, para consulta pelo script, e assim, pontuar as cidades capitais.
 
-Este script em PHP foi desenvolvido de uma forma simples para facilitar o entendimento dos algoritmos e aceita variáveis externas, pelo método GET, para definir a largura e a altura do mapa. Ex.: mapamundi.php?largura=1000&altura=500
+Este script em PHP foi desenvolvido de uma forma simples para facilitar o entendimento dos algoritmos e aceita variáveis externas, pelo método GET, para definir a largura e a altura do mapa, como também o algoritmo de projeção (equidistante, sinusoidal, Kavrayskiy VII e Robinson). Ex.: mapamundi.php?largura=1000&altura=500&projecao=r
 
 ### O mapa
 
@@ -18,6 +18,16 @@ Este script em PHP foi desenvolvido de uma forma simples para facilitar o entend
 
 - COWBURN, P. e col. *Manual do PHP*. PHP Documentation Group. 25 de Dezembro de 2020. Disponível em: <https://www.php.net/manual/pt_BR/index.php>
 
+- ESRI *Understanding Map Projections*. GIS by ESRI. Environmental Systems Research Institute, 2000. Disponível em: <http://downloads2.esri.com/support/documentation/ao_/710Understanding_Map_Projections.pdf>
+
+- FENNA, D. *Cartographic Science: A Compendium of Map Projections, with Derivations*. CRC Press, Taylor & Francis Group, 2007.
+
+- GOLDBERG, D. M.; GOTT, J. R. *Flexion and Skewness in Map Projections of the Earth*. Cartographica: The International Journal for Geographic Information and Geovisualization. Volume 42 Issue 4, pp. 297-318, December 18, 2007.
+
+- HOOIJBERG, M. *Practical Geodesy: Using Computers*. Springer-Verlag, Berlin, Heidelberg, 1997.
+
+- IPBUKER, C. *A computational approach to the Robinson projection*. Survey Review. Volume 38, Issue 297, pp. 204-217, July 2005.
+
 - KELSO, N. V. *Natural Earth vector*. Disponível em: <https://github.com/nvkelso/natural-earth-vector>
 
 - KELSO, N. V.; PATTERSON, T. *Natural Earth*. Disponível em: <https://www.naturalearthdata.com/>
@@ -26,10 +36,15 @@ Este script em PHP foi desenvolvido de uma forma simples para facilitar o entend
 
 - NIMA *Technical Report 8350.2, Department of Defense World Geodetic System 1984: Its Definition and Relationships with Local Geodetic Systems, Third Edition, Amendment 1*. Geodesy and Geophysics Department, National Imagery and Mapping Agency. January 3, 2000.
 
+- ŠAVRIČ, B.; PATTERSON, T.; JENNY, B. *The Natural Earth II world map projection*. International Journal of Cartography, 
+Vol. 1, No. 2, pp. 123–133, 2015.
+
 - SimpleMaps.com *How to import a CSV file into MySQL*. Tutorials and Articles. Disponível em: <https://simplemaps.com/resources/import-csv-mysql>
 
 - SimpleMaps.com *World Cities Database*. SimpleMaps Worldcities Basic v1.73. Disponível em: <https://simplemaps.com/data/world-cities>
 
-- SNYDER, J. P. *Map Projections A Working Manual*, U.S. Geological Survey Professional Paper 1395, Supersedes USGS Bulletin 1532, United States Government Printing Office, Washington, 1987.
+- SNYDER, J. P. *Flattening the Earth: Two thousand years of map projections*. University of Chicago Press, Chicago, 1993.
+
+- SNYDER, J. P. *Map Projections - A Working Manual*, U.S. Geological Survey Professional Paper 1395, Supersedes USGS Bulletin 1532, United States Government Printing Office, Washington, 1987.
 
 - W3C *Scalable Vector Graphics (SVG) 1.1 (Second Edition)*, W3C Recommendation 16 August 2011. Disponível em: <https://www.w3.org/TR/SVG11/>
--- php/mapamundi.php
+++ php/mapamundi.php
@@ -5,8 +5,9 @@ ini_set('display_errors', 1);
 
 $largura = filter_input(INPUT_GET, 'largura', FILTER_VALIDATE_INT, array('options' => array('default' => 1200, 'min_range' => 720, 'max_range' => 3840)));
 $altura = filter_input(INPUT_GET, 'altura', FILTER_VALIDATE_INT, array('options' => array('default' => 566, 'min_range' => 480, 'max_range' => 2160)));
+$projecao = filter_input(INPUT_GET, 'projecao', FILTER_VALIDATE_REGEXP, array('options' => array('default' => 'r', 'regexp' => '/[ekrs]/')));
 
-$dsn = 'mysql:host=192.168.1.10;dbname=world;charset=utf8';
+$dsn = 'mysql:host=localhost;dbname=world;charset=utf8';
 $user = 'usuario';
 $pass = 'senha';
 $pdo = new PDO($dsn, $user, $pass);
@@ -17,14 +18,15 @@ $query = "SELECT city, lat, lng, country, id FROM worldcities
           ORDER BY population";
 $result = $pdo->query($query);
 
-echo montarPagina($result, $largura, $altura);
+echo montarPagina($result, $largura, $altura, $projecao);
 
-function montarPagina(object $result, int $largura, int $altura): string
+function montarPagina(object $result, int $largura, int $altura, string $projecao): string
 {
     $svg = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="' . $largura.'" height="' . $altura . '">' . PHP_EOL;
     $svg .= '<rect x="0" y="0" width="' . $largura . '" height="' . $altura . '" fill="none" stroke="rgb(150,150,150)" stroke-width="1" />' . PHP_EOL;
-    $svg .= exibirMundo($largura, $altura);
-    $svg .= exibirCidades($result, $largura, $altura);
+    $svg .= exibirMundo($largura, $altura, $projecao);
+    $svg .= exibirCidades($result, $largura, $altura, $projecao);
+    $svg .= exibirGrade($largura, $altura, $projecao);
     $svg .= '</svg>' . PHP_EOL;
 
     $html = '<!DOCTYPE html>' . PHP_EOL . '<html>' . PHP_EOL . '<body>' . PHP_EOL;
@@ -34,7 +36,7 @@ function montarPagina(object $result, int $largura, int $altura): string
     return $html;
 }
 
-function exibirCidades(object $result, int $largura, int $altura): string
+function exibirCidades(object $result, int $largura, int $altura, string $projecao): string
 {
     $svg = '<g fill="rgb(33,42,116)" stroke-width="0" onmouseover="evt.target.setAttribute(\'fill\', \'blue\');" onmouseout="evt.target.setAttribute(\'fill\',\'red\');">' . PHP_EOL;
     
@@ -42,7 +44,7 @@ function exibirCidades(object $result, int $largura, int $altura): string
         while ($cidade = $result->fetch(PDO::FETCH_ASSOC)) {
             $latitude = $cidade['lat'];
             $longitude = $cidade['lng'];
-            $ponto = converterGeoPixel($latitude, $longitude, $largura, $altura);
+            $ponto = converterGeoPixel($latitude, $longitude, $largura, $altura, $projecao);
             $titulo = $cidade['city'] . ' (' . $cidade['country'] . ')';
             $svg .= '<circle id="'.$cidade['id'].'" cx="' . $ponto['x'] . '" cy="' . $ponto['y'] . '" r="3"><title>' . $titulo . '</title></circle>' . PHP_EOL;
         }
@@ -51,36 +53,76 @@ function exibirCidades(object $result, int $largura, int $altura): string
     return $svg;
 }
 
-function converterGeoPixel(float $latitude, float $longitude, int $largura, int $altura): array
+function converterGeoPixel(float $latitude, float $longitude, int $largura, int $altura, string $projecao): array
 {
+    if ($projecao == 'e') { // plate carrée // equirectangular projection
+        $radiano = false;
+    } elseif ($projecao == 'k') { // Kavrayskiy VII projection
+        $radiano = false;
+    } elseif ($projecao == 'r') { // Robinson projection
+        $radiano = true;
+    } elseif ($projecao == 's') { // sinusoidal projection
+        $radiano = false;
+    }
     $centro = coordenarCentro($largura, $altura);
-    $modulo = calcularModulo($largura, $altura);
+    $modulo = calcularModulo($largura, $altura, $radiano);
     
-    $x = floor($centro['x'] + ($longitude * $modulo));
-    $y = floor($centro['y'] - ($latitude * $modulo));
+    if ($projecao == 'r') {
+        $latitude = deg2rad($latitude);
+        $longitude = deg2rad($longitude);
+        if ($latitude >= 0) {
+            $s = -1;
+        } else {
+            $s = 1;
+        }
+        $x = floor($centro['x'] + ((2.6666 - 0.367 * pow($latitude, 2) - 0.150 * pow($latitude, 4) + 0.0379 * pow($latitude, 6)) * $longitude / pi()) * $modulo);
+        $y = floor($centro['y'] - (0.96047 * $latitude - 0.00857 * $s * pow(abs($latitude), 6.41)) * $modulo);
+    } else {
+        $fatorpolo = calcularFatorPolo($latitude, $projecao);
+        $x = floor($centro['x'] + ($longitude * $modulo * $fatorpolo));
+        $y = floor($centro['y'] - ($latitude * $modulo));
+    }
     
     return array('x' => $x, 'y' => $y);
 }
 
 function coordenarCentro(int $largura, int $altura): array
 {
-    $centro = array();
-    $centro['x'] = $largura / 2;
-    $centro['y'] = $altura / 2;
+    $centro = array('x' => $largura / 2,
+                    'y' => $altura / 2);
     return $centro;
 }
 
-function calcularModulo(int $largura, int $altura): float
+function calcularModulo(int $largura, int $altura, bool $radiano = false): float
 {
+    $paralelo = 360;
+    $meridiano = 180;
+    if ($radiano) {
+        $paralelo = deg2rad($paralelo);
+        $meridiano = deg2rad($meridiano);
+    }
     if ($largura / $altura < 2) {
-        $modulo = $largura / 360;
+        $modulo = $largura / $paralelo;
     } else {
-        $modulo = $altura / 180;
+        $modulo = $altura / $meridiano;
     }
     return $modulo;
 }
 
-function exibirMundo(int $largura, int $altura): string
+function calcularFatorPolo(float $latitude, string $projecao): float
+{
+    if ($projecao == 'e') {
+        return 1; // plate carrée // equirectangular projection
+    } elseif ($projecao == 'k') {
+        return abs(3/2 * sqrt(1/3 - pow(deg2rad($latitude)/pi(), 2))); // Kavrayskiy VII projection
+    } elseif ($projecao == 's') {
+        return abs(cos(deg2rad($latitude))); // sinusoidal projection
+    }
+    ////return abs((abs($latitude) / 90) - 1); // 
+    ////return abs(((abs($latitude) - abs($latitude) * 50 / 100) / 90) - 1); // redução de 50%
+}
+
+function exibirMundo(int $largura, int $altura, string $projecao): string
 {
     $svg = '<g fill="rgb(200,200,200)" fill-rule="nonzero" stroke="rgb(150,150,150)" stroke-width="1">' . PHP_EOL;
     
@@ -92,7 +134,7 @@ function exibirMundo(int $largura, int $altura): string
         if ($pais['geometry']['type'] == 'MultiPolygon') {
             foreach ($coordenadas as $multi) {
                 foreach ($multi as $poligono) {
-                    $pontos = extrairPoligono($poligono, $largura, $altura);
+                    $pontos = extrairPoligono($poligono, $largura, $altura, $projecao);
                     if (substr_count($pontos, ' ') > 1) {
                         $svg .= '<polygon points="' . $pontos . '" />' . PHP_EOL;
                     }
@@ -100,7 +142,7 @@ function exibirMundo(int $largura, int $altura): string
             }
         } elseif ($pais['geometry']['type'] == 'Polygon') {
             foreach ($coordenadas as $poligono) {
-                $pontos = extrairPoligono($poligono, $largura, $altura);
+                $pontos = extrairPoligono($poligono, $largura, $altura, $projecao);
                 if (substr_count($pontos, ' ') > 1) {
                     $svg .= '<polygon points="' . $pontos . '" />' . PHP_EOL;
                 }
@@ -111,14 +153,14 @@ function exibirMundo(int $largura, int $altura): string
     return $svg;
 }
 
-function extrairPoligono(array $poligono, int $largura, int $altura): string
+function extrairPoligono(array $poligono, int $largura, int $altura, string $projecao): string
 {
     $pontos = '';
     $anterior = array('x' => 0, 'y' => 0);
     foreach ($poligono as $coordenada) {
         $latitude = $coordenada[1];
         $longitude = $coordenada[0];
-        $ponto = converterGeoPixel($latitude, $longitude, $largura, $altura);
+        $ponto = converterGeoPixel($latitude, $longitude, $largura, $altura, $projecao);
         if ($ponto['x'] != $anterior['x'] or $ponto['y'] != $anterior['y']) {
             if (!empty($pontos)) {
                 $pontos .= ' ';
@@ -135,5 +177,92 @@ function carregarMundo(): array
     $json = file_get_contents('json/ne_110m_admin_0_countries_lakes.geojson');
     $array = json_decode($json, true);
     return $array;
+}
+
+function exibirGrade(int $largura, int $altura, string $projecao): string
+{
+    $svg = '<g fill="none" stroke="lightgray" stroke-width="1">' . PHP_EOL;
+    
+    $svg .= exibirParalelos($largura, $altura, $projecao);
+    $svg .= exibirMeridianos($largura, $altura, $projecao);
+    $svg .= exibirCirculos($largura, $altura, $projecao);
+    $svg .= exibirEquador($largura, $altura, $projecao);
+    $svg .= exibirGreenwich($largura, $altura, $projecao);
+    
+    $svg .= '</g>' . PHP_EOL;
+    return $svg;
+}
+
+function exibirParalelos(int $largura, int $altura, string $projecao): string
+{
+    $svg = '';
+    $paralelos = array(15, 30, 45, 60, 75, 90);
+    foreach ($paralelos as $latitude) {
+        $nw = converterGeoPixel($latitude, 180, $largura, $altura, $projecao);
+        $ne = converterGeoPixel($latitude, -180, $largura, $altura, $projecao);
+        $svg .= '<line x1="' . $ne['x'] . '" y1="' . $ne['y'] . '" x2="' . $nw['x'] . '" y2="' . $nw['y'] . '" />' . PHP_EOL;
+        $sw = converterGeoPixel(-$latitude, 180, $largura, $altura, $projecao);
+        $se = converterGeoPixel(-$latitude, -180, $largura, $altura, $projecao);
+        $svg .= '<line x1="' . $se['x'] . '" y1="' . $se['y'] . '" x2="' . $sw['x'] . '" y2="' . $sw['y'] . '" />' . PHP_EOL;
+    }
+    return $svg;
+}
+
+function exibirMeridianos(int $largura, int $altura, string $projecao): string
+{
+    $svg = '';
+    $meridianos = array(15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180);
+    foreach ($meridianos as $longitude) {
+        $ponto = converterGeoPixel(-90, $longitude, $largura, $altura, $projecao);
+        $svg .= '<path d="M' . $ponto['x'] . ',' . $ponto['y'];
+        for ($latitude = -89; $latitude <= 90; $latitude++) {
+            $ponto = converterGeoPixel($latitude, $longitude, $largura, $altura, $projecao);
+            $svg .= ' L' . $ponto['x'] . ',' . $ponto['y'];
+        }
+        $svg .= '" />' . PHP_EOL;
+        $ponto = converterGeoPixel(-90, -$longitude, $largura, $altura, $projecao);
+        $svg .= '<path d="M' . $ponto['x'] . ',' . $ponto['y'];
+        for ($latitude = -89; $latitude <= 90; $latitude++) {
+            $ponto = converterGeoPixel($latitude, -$longitude, $largura, $altura, $projecao);
+            $svg .= ' L' . $ponto['x'] . ',' . $ponto['y'];
+        }
+        $svg .= '" />' . PHP_EOL;
+    }
+    return $svg;
+}
+
+function exibirCirculos(int $largura, int $altura, string $projecao): string
+{
+    $svg = '';
+    
+    $tw = converterGeoPixel(23.43656, 180, $largura, $altura, $projecao);
+    $te = converterGeoPixel(23.43656, -180, $largura, $altura, $projecao);
+    $svg .= '<line x1="' . $te['x'] . '" y1="' . $te['y'] . '" x2="' . $tw['x'] . '" y2="' . $tw['y'] . '" stroke-dasharray="10,10" />' . PHP_EOL;
+    $tw = converterGeoPixel(-23.43656, 180, $largura, $altura, $projecao);
+    $te = converterGeoPixel(-23.43656, -180, $largura, $altura, $projecao);
+    $svg .= '<line x1="' . $te['x'] . '" y1="' . $te['y'] . '" x2="' . $tw['x'] . '" y2="' . $tw['y'] . '" stroke-dasharray="10,10" />' . PHP_EOL;
+    
+    $cw = converterGeoPixel(66.5622, 180, $largura, $altura, $projecao);
+    $ce = converterGeoPixel(66.5622, -180, $largura, $altura, $projecao);
+    $svg .= '<line x1="' . $ce['x'] . '" y1="' . $ce['y'] . '" x2="' . $cw['x'] . '" y2="' . $cw['y'] . '" stroke-dasharray="10,10" />' . PHP_EOL;
+    $cw = converterGeoPixel(-66.5622, 180, $largura, $altura, $projecao);
+    $ce = converterGeoPixel(-66.5622, -180, $largura, $altura, $projecao);
+    $svg .= '<line x1="' . $ce['x'] . '" y1="' . $ce['y'] . '" x2="' . $cw['x'] . '" y2="' . $cw['y'] . '" stroke-dasharray="10,10" />' . PHP_EOL;
+    
+    return $svg;
+}
+
+function exibirEquador(int $largura, int $altura, string $projecao): string
+{
+    $w = converterGeoPixel(0, 180, $largura, $altura, $projecao);
+    $e = converterGeoPixel(0, -180, $largura, $altura, $projecao);
+    return '<line x1="' . $e['x'] . '" y1="' . $e['y'] . '" x2="' . $w['x'] . '" y2="' . $w['y'] . '" />' . PHP_EOL;
+}
+
+function exibirGreenwich(int $largura, int $altura, string $projecao): string
+{
+    $n = converterGeoPixel(90, 0, $largura, $altura, $projecao);
+    $s = converterGeoPixel(-90, 0, $largura, $altura, $projecao);
+    return '<line x1="' . $n['x'] . '" y1="' . $n['y'] . '" x2="' . $s['x'] . '" y2="' . $s['y'] . '" />' . PHP_EOL;
 }
 
